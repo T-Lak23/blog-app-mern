e14d6d6870cd51a1e8dff62cb5125e861d4c1a87
T-Lak23 <tilak230412@gmail.com>
2025-09-09 18:14:46 +0530

    utils changed backend

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,8 +7,8 @@ export const generateTokenAndSetCookie = (res, userId, role) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "None",
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
 
